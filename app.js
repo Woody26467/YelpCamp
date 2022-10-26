249cb3a22cef2bf5ae8e3a8782111bda26fdf37c
Andy Woodward <andy.woodward1967@gmail.com>
2022-10-26 14:42:36 +0100

    make list items links and create a route for show page and create the view page based on id

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 })
 
+// Set up route for show page
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+})
+
 app.listen(3000, () => {
     console.log('Serving on port 3000');
 })
